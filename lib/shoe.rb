6b82418ee7972a4559a212d5ab5500d54ac365f1
Oscar Ore <oscar.ore2011@gmail.com>
2020-02-07 23:59:07 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,15 @@
 # Make your shoe class here!
 class Shoe 
-  
+    attr_accessor :brand, :color, :size, :material, :condition 
+  attr_reader :brand 
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def cobble
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
 end 
